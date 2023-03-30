0a327a5722b4dd273a66c26f9906f22f52fdc5d8
Oignnon - <td665pro@pm.me>
2023-03-31 01:52:04 +0200

    Spending a lot of time on CSS

---
+++ src/components/Body.js
@@ -0,0 +1,52 @@
+import React from "react";
+import tyler from "./images/tyler-group.jpg";
+
+export default function Body() {
+  return (
+    <div className="full">
+      <div className="container-card">
+        <div className="header">
+          <img className="photo" src={tyler} alt="" width="400px" />
+          <h1>Tyler Durnion</h1>
+          <h3>Web Developer</h3>
+          <p>
+            <a href="https://portfolio-tylerdurnion.netlify.app/">
+              portfolio-tylerdurnion.netlify.app.
+            </a>
+          </p>
+          <div className="button-container">
+          <button>
+            <a href="mailto:td665@pm.me" target="_blank" rel="noreferrer">
+              Email
+            </a>
+          </button>
+          <button>
+            <a
+              href="https://www.linkedin.com/in/tylerdurnion/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </button>
+          </div>
+        </div>
+        <div className="text">
+          <h1>About</h1>
+          <p>
+            I am a back-end developer looking to broden my horizons and learn
+            more about the front-end, Javascript, React and anything I can get
+            my hands on really. I enjoy building websites and apps that have
+            meaning to me and my clients. I am always look to learn.
+          </p>
+          <h1>Interests</h1>
+          <p>
+            Guitar is my main passion. I love carpentry, cooking, reading. Savy
+            language learner speaking French, Spanish and a little bit of
+            Arabic. Love to travel, cycle and meet new people.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css';
+import Body from "./components/Body.js"
+
+
+function App() {
+  return (
+    <>
+    <Body />
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <App />
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
